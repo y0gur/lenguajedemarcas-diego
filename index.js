2125f138432f0c122c62e6edeeed651dce91451d
Diego Delgado <123065732+y0gur@users.noreply.github.com>
2024-05-09 22:07:18 +0200

    v0.2
    botones de sumar y restar completados
    y siendo estilados

--- index.js
+++ index.js
@@ -10,8 +10,38 @@ function suma1(){
     countEl.textContent = count 
 }
 
+function suma2(){
+    count = count+2   //count += 2 
+    countEl.textContent = count 
+}
+
+function suma5(){
+    count = count+5   //count += 5 
+    countEl.textContent = count 
+}
+
+function suma10(){
+    count = count+10   //count += 10 
+    countEl.textContent = count 
+}
+
 function resta1(){
     count = count-1   //count -= 1 
+    countEl.textContent = count 
+}
+
+function resta2(){
+    count = count-2   //count -= 2 
+    countEl.textContent = count 
+}
+
+function resta5(){
+    count = count-5   //count -= 5 
+    countEl.textContent = count 
+}
+
+function resta10(){
+    count = count-10   //count -= 10 
     countEl.textContent = count 
 }
 
@@ -20,5 +50,7 @@ function save(){
     saveEl.textContent += countStr
     countEl.textContent = count 
     count = 0
+    countEl.textContent = count
 }
+
 
